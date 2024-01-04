0457b3c1a87fed429217ebfcb60c39e91a397447
zivmax <108971427+zivmax@users.noreply.github.com>
2024-01-04 11:19:52 +0800

    improve code sytle

--- arguments.py
+++ arguments.py
@@ -1,26 +1,32 @@
-import argparse  
+import argparse
 parser = argparse.ArgumentParser(description="Submit code to Hyrod OJ")
-parser.add_argument("-uca",
-                    "--use-config-account",
-                    action="store_true",
-                    help="Use account information from config file instead of prompting",
-                    default=True
-                    )
-parser.add_argument("-pid",
-                    "--problem-id",
-                    type=str,
-                    help="Specify the problem ID to submit",
-                    default=""
-                    )
-parser.add_argument('-np', '--noPrepare',
-                default=False,
-                action="store_true",
-                help='Skip the prepare procedure.')
+parser.add_argument(
+    "-uca",
+    "--use-config-account",
+    action="store_true",
+    help="Use account information from config file instead of prompting",
+    default=True
+)
+parser.add_argument(
+    "-pid",
+    "--problem-id",
+    type=str,
+    help="Specify the problem ID to submit",
+    default=""
+)
+parser.add_argument(
+    "-np",
+    "--noPrepare",
+    default=False,
+    action="store_true",
+    help="Skip the prepare procedure.")
 
-parser.add_argument('-d', '--dev',
-                    default=False,
-                    action="store_true",
-                    help='Run in developping mode, printing the extra info.')
+parser.add_argument(
+    "-d",
+    "--dev",
+    default=False,
+    action="store_true",
+    help="Run in developping mode, printing the extra info.")
 parser.add_argument(
     "-I",
     "--init",
